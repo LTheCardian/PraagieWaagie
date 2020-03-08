87c4b5a0624baafae21af28edd4a3248e622b26b
Gio <531406@student.glu.nl>
2020-03-08 21:20:18 +0100

    released the alpha of the chat feature.

--- Client/src/Components/screens/Chat/SidePanel/SidePanel.js
+++ Client/src/Components/screens/Chat/SidePanel/SidePanel.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import {Menu} from 'semantic-ui-react'
-import Channels from './Channels'
+import React from "react";
+import { Menu, Button } from "semantic-ui-react";
+import {Link} from 'react-router-dom'
+import Channels from "./Channels";
 
-class SidePanel extends React.Component{
-    render(){
-        const {currentUser} = this.props
-        return(
-            <Channels currentUser={currentUser}/>    
-        )
-    }
+class SidePanel extends React.Component {
+  render() {
+    const { currentUser} = this.props;
+
+    return (
+    //   <Menu
+    //     size="large"
+    //     inverted
+    //     fixed="left"
+    //     vertical
+    //     style={{fontSize: "1.2rem"}}
+    //     className="menuUserpanel"
+      
+    //   >
+        <Channels currentUser={currentUser}/>
+    //   </Menu>
+    );
+  }
 }
 
-export default SidePanel
+export default SidePanel;
